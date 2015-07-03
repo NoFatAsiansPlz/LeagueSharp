f207daf17375399f726edb028f0ae3a04f882c26
NoFatAsiansPlz <leonevets@gmail.com>
2015-07-03 13:57:23 -0400

    Update Program.cs

--- OP Macro/Program.cs
+++ OP Macro/Program.cs
@@ -14,8 +14,8 @@ namespace HaydariGeceler_cici_wipi_ENG
         private static LeagueSharp.Common.Menu haydarigeceler;
         public static bool duramk = false;
         public static float gameTime1 = 0;
-        private static string IsMe = "glhf ";
-        private static string riot = " [Riot Games®] We added a new feature! Press enter twice and type fiddlix";
+        private static string IsMe = "glhf";
+        private static string riot = "[Riot Games®] We added a new feature! Press enter twice and type fiddlix";
         private static string message;
 
         private static void Main(string[] args)
