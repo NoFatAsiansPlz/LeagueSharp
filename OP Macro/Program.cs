332c6c6f603192831115e023feb2ab4fc6b93c7b
NoFatAsiansPlz <leonevets@gmail.com>
2015-07-03 01:25:48 -0400

    Update Program.cs

--- OP Macro/Program.cs
+++ OP Macro/Program.cs
@@ -88,7 +88,7 @@ namespace HaydariGeceler_cici_wipi_ENG
                 if (haydarigeceler.Item("XDyaz").GetValue<KeyBind>().Active)
                     if (duramk == false)
                     {
-                        var message = string.Format("/all {0}{1}  {2}", IsMe, new string('   ', 65), riot);
+                        var message = string.Format("/all {0}{1}  {2}", IsMe, new string('  ', 65), riot);
                         Game.Say(message);
                         duramk = true;
                         gameTime1 = Game.Time + 1;
