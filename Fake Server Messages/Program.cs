eb46a53d40751a98f0ae935f49b46ef7167dee91
NoFatAsiansPlz <leonevets@gmail.com>
2015-07-02 21:49:31 -0400

    Update Program.cs

--- Fake Server Messages/Program.cs
+++ Fake Server Messages/Program.cs
@@ -3,11 +3,6 @@ using System.Collections;
 using System.Linq;
 using LeagueSharp;
 using LeagueSharp.Common;
-using SharpDX;
-using System.Drawing;
-using Color = System.Drawing.Color;
-using System.Collections.Generic;
-using System.Threading;
 
 namespace Fake_Server_Messages
 {
