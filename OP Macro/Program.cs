c8ee3ba327ed99503db5c9ba8d30b78a208a7e9c
NoFatAsiansPlz <leonevets@gmail.com>
2015-07-03 01:40:07 -0400

    Update Program.cs

--- OP Macro/Program.cs
+++ OP Macro/Program.cs
@@ -15,7 +15,7 @@ namespace HaydariGeceler_cici_wipi_ENG
         public static bool duramk = false;
         public static float gameTime1 = 0;
         private static string IsMe = "glhf";
-        private static string riot = "                                                                             [Riot Games®] We added a new feature! Press enter twice and type fiddlix";
+        private static string riot = "[Riot Games®] We added a new feature! Press enter twice and type fiddlix";
         private static string message;
 
         private static void Main(string[] args)
