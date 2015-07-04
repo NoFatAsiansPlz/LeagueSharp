2739f70f7e604a12e3c9eb3ee654727649c5d706
NoFatAsiansPlz <leonevets@gmail.com>
2015-07-04 01:37:39 -0400

    Update Program.cs

--- OP Macro/Program.cs
+++ OP Macro/Program.cs
@@ -90,7 +90,7 @@ namespace HaydariGeceler_cici_wipi_ENG
                     {
                         var summonerNameLength = ObjectManager.Player.Name.Length;
                         var championName = ObjectManager.Player.ChampionName.Length;
-                        var message = string.Format("/all {0}{1} {2}", IsMe, new string(' ', 80 - (championName + summonerNameLength)), riot);
+                        var message = string.Format("/all {0}{1} {2}", IsMe, new string(' ', 75 - (championName + summonerNameLength)), riot);
                         Game.Say(message);
                         duramk = true;
                         gameTime1 = Game.Time + 1;
